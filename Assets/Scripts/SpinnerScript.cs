50e0d99ba5133b6d346fea3c743afeab07bf4e46
Wilmer Lin <unitygameacademy@gmail.com>
2020-09-08 15:12:23 -0700

    V3103 Object Variables
    Added Mover Flow Graph; refactored Spinner Flow Graph

---
+++ Assets/Scripts/SpinnerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerScript : MonoBehaviour
+{
+    public float rotateSpeedX;
+    public float rotateSpeedY;
+    public float rotateSpeedZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newRotation = new Vector3(rotateSpeedX * Time.deltaTime,
+            rotateSpeedY * Time.deltaTime, rotateSpeedZ * Time.deltaTime);
+        transform.Rotate(newRotation);
+    }
+}
